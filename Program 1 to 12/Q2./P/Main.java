c7320bbde320bcfcf35ab79aa7c1ab97e4bbafd2
Khushal_88044 <68191677+CodeKhushal@users.noreply.github.com>
2021-06-26 18:27:31 +0530

    Update Main.java

--- Program 1 to 12/Q2./P/Main.java
+++ Program 1 to 12/Q2./P/Main.java
@@ -1,1 +1,12 @@
+package P;
 
+import P1.TwoDim;
+import P2.ThreeDim;
+public class Main {
+    public static void main(String[] args) {
+        TwoDim ref1 = new TwoDim(8, 12);
+        System.out.println(ref1.toString());
+        ThreeDim ref2 = new ThreeDim(6, 9, 18);
+        System.out.println(ref2.toString());
+    }
+}
